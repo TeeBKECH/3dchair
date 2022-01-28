56b454b1b1c546568fb0243b993a91696052c8a0
TeeBKECH <t.e.r.r.a.nik566@gmail.com>
2022-01-28 15:46:46 +0300

    fixes

--- src/canvas/Scene.jsx
+++ src/canvas/Scene.jsx
@@ -16,8 +16,8 @@ export const Scene = ({newMaterialOpt}) => {
     // Scene configuration;
     useEffect(() => {
         const directionalLight = scene.children[1];
-        scene.background = new THREE.Color(0xf1f1f1);
-        scene.fog = new THREE.Fog(0xf1f1f1, 20, 100);
+        scene.background = new THREE.Color(0x19afca);
+        scene.fog = new THREE.Fog(0x19afca, 20, 100);
         camera.fov = 50;
         directionalLight.shadow.mapSize = new THREE.Vector2(1024, 1024)
         shadowMap.enabled = true;
